f331e14256a941f5e7f668f6a3f377e152b8b57e
Pavel Potoček <pavel.potocek@certicon.cz>
2020-06-06 18:19:13 +0200

    Fix the work blocking the UI

--- app.py
+++ app.py
@@ -12,7 +12,7 @@ def app():
 		event, key = yield from create_main_view()
 		if event == "Nice Feature":
 			from src.features.nice_feature.nice_feature import NiceFeature
-			t_nice_feature = Thread(target=NiceFeature().run(), daemon=True)
+			t_nice_feature = Thread(target=NiceFeature().run, daemon=True)
 			t_nice_feature.start()
 			running_features.append({"attack_name": "Token Checker", "timestamp": datetime.datetime.now(), "thread_obj": t_nice_feature})
 		elif event == "Quit":
