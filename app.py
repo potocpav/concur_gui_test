7249938f1776a4007093709d74ec139417af52e2
Pavel Potoček <pavel.potocek@certicon.cz>
2020-06-06 19:14:13 +0200

    Hold state inside a class. Show work result as text.

--- app.py
+++ app.py
@@ -7,39 +7,43 @@ from src.features.nice_feature.nice_feature_gui import nice_feature_gui
 from src.features.nice_feature.nice_feature import do_work
 
 
-
+class State:
+	def __init__(self):
+		self.future = None
+		self.window_visible = True
+		self.work_id = 0
+		self.work_result = None
 
 
 def app():
 	executor = ThreadPoolExecutor(100)
-	running_future = None
-	window_visible = True
-	i = 0
+	state = State()
 
 	while True:
 		key, value = yield from c.orr([
-			create_main_view(),
-			c.optional(window_visible, nice_feature_gui),
-			c.tag("Result", c.Block(running_future)) if running_future else c.nothing()
+			create_main_view(state),
+			c.optional(state.window_visible, nice_feature_gui),
+			c.tag("Result", c.Block(state.future)) if state.future else c.nothing()
 			])
 
 		if key == "Nice Feature":
-			if running_future is None:
+			if state.future is None:
 				print("Starting computation...")
-				running_future = executor.submit(do_work, i)
-				i += 1
-				window_visible = False
+				state.future = executor.submit(do_work, state.work_id)
+				state.work_id += 1
+				state.window_visible = False
 			else:
 				print("Computation already running.")
 
 		elif key == "Result":
-			running_future = None
+			state.future = None
+			state.work_result = value
 			print(f"Done. Result: {value}")
 
 		elif key == "Show Window":
-			window_visible = True
+			state.window_visible = True
 		elif key == "Close":
-			window_visible = False
+			state.window_visible = False
 
 		elif key == "Quit":
 			break
@@ -48,9 +52,15 @@ def app():
 		yield
 
 
-def create_main_view():
-	main_window = c.orr([
+def create_main_view(state):
+	if state.future:
+		status_text = "Working..."
+	elif state.work_result is not None:
+		status_text = f"Result: {state.work_result}"
+	else:
+		status_text = ""
 
+	main_window = c.orr([
 		c.main_menu_bar(widget=c.orr([
 			c.menu(label="File", widget=c.orr([
 				c.menu_item("Do things"),
@@ -59,6 +69,7 @@ def create_main_view():
 		])),
 
 		c.button("Show Window"),
+		c.text(status_text),
 		c.button("Quit"),
 		c.key_press("Quit", ord('Q'), ctrl=True),
 	])
